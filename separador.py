4ee8ee621dcf3c87c1823e6d77dd005030f6a459
vlcanesin <vlcanesin@gmail.com>
2022-07-07 22:37:58 -0300

    Alteração no nome dos artigos gerados (terceiro commit)

--- separador.py
+++ separador.py
@@ -1,17 +1,21 @@
 import json
 
+###---------------------------###
+###-------- SEPARADOR --------###
+###---------------------------###
+
+### Como utilizar:
+### 1. Gerar lista de artigos usando script.py
+### 2. Verificar se os artigos gerados estão no arquivo jsonForScript.json
+### 3. Rodar programa
+### 4. Copiar artigos gerados para a pasta de artigos do site
+
 f = open('jsonForScript.json', 'r', encoding='utf8')
 file = json.load(f)
 
 for item in file:
     print(item)
-    if (2020 - item["ano"]) == 10:
-        prec = 'A'
-    elif (2020 - item["ano"]) == 11:
-        prec = 'B'
-    else:
-        prec = (2020 - item["ano"])
-    arq_name = (str(item["ano"]) + '/' + str(prec) + '-' + 'artigos' + '-' + str(item["ano"]) + '-' + str(item["nome"]).replace(':', '').replace('/', '-').replace('?', '') + '.json')
+    arq_name = (str(item["ano"]) + '/' + 'artigo' + '-' + str(item["ano"]) + '-' + str(item["nome"]).replace(':', '').replace('/', '-').replace('?', '') + '.json')
     if(len(arq_name) > 64):
         arq_name = arq_name[:64] + '.json'
     with open(arq_name, 'w', encoding='utf8') as file:
